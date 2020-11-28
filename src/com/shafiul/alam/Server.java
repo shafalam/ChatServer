eebf27f8e65e7623627cac0297f20a6e2093f233
Shafiul Alam <shafiulalam16@gmail.com>
2020-11-28 21:52:36 +0100

    created gitignore

--- src/com/shafiul/alam/Server.java
+++ src/com/shafiul/alam/Server.java
@@ -13,19 +13,19 @@ import java.util.Queue;
 
 public class Server {
     private ArrayList<PrintWriter> totalClients;
-    //private PriorityQueue<String> totalMessages;
+    // private PriorityQueue<String> totalMessages;
     ArrayList<String> totalMessages;
     private boolean startFlag;
 
-    public void startServer(){
+    public void startServer() {
         totalClients = new ArrayList<PrintWriter>();
-        //totalMessages = new PriorityQueue<>();
+        // totalMessages = new PriorityQueue<>();
         totalMessages = new ArrayList<String>();
 
-        try{
+        try {
             ServerSocket serverSocket = new ServerSocket(5000);
             int socketId = 0;
-            while(true){
+            while (true) {
                 Socket socket = serverSocket.accept();
                 PrintWriter writer = new PrintWriter(socket.getOutputStream());
                 totalClients.add(writer);
@@ -33,47 +33,50 @@ public class Server {
                 new ClientReader(socket, socketId).start();
                 System.out.println("Got a connection");
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("IOException occurred: " + e.getMessage());
             e.printStackTrace();
         }
     }
-    class ClientReader extends Thread{
+
+    class ClientReader extends Thread {
         Socket socket;
         String message;
         int socketId;
-        public ClientReader(Socket socket, int socketId){
+
+        public ClientReader(Socket socket, int socketId) {
             this.socket = socket;
             this.socketId = socketId;
         }
+
         @Override
-        public void run(){
+        public void run() {
             message = "";
-            try{
+            try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                while (true ){
+                while (true) {
                     message = reader.readLine();
-                    if(!message.equals(null)) {
+                    if (!message.equals(null)) {
                         tellEveryOne(message, socketId);
-                        System.out.println("Message received from client "+ socketId + ": " + message);
+                        System.out.println("Message received from client " + socketId + ": " + message);
                     }
                 }
 
-            }catch(IOException e){
+            } catch (IOException e) {
                 System.out.println("IOException occurred: " + e.getMessage());
             }
 
         }
     }
 
-    public void tellEveryOne(String message, int clientId){
+    public void tellEveryOne(String message, int clientId) {
         Iterator it = totalClients.iterator();
-        while (it.hasNext()){
-            try{
+        while (it.hasNext()) {
+            try {
                 PrintWriter writer = (PrintWriter) it.next();
                 writer.println("Friend: " + clientId + ": " + message);
                 writer.flush();
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
